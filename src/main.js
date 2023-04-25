67f0883e5064b4878ed7f87df5df9c51348448e6
Tianzhu_Wang <stephen_einstein@icloud.com>
2023-04-25 15:07:30 +0800

    ready for publishing

--- src/main.js
+++ src/main.js
@@ -3,10 +3,6 @@ import App from './App.vue'
 import router from './router'
 import store from '@/store'
 
-import { library } from '@fortawesome/fontawesome-svg-core'
-import { FontAwesomeIcon } from "@fortawesome/vue-fontawesome"
-import { faUserSecret } from '@fortawesome/free-solid-svg-icons'
-
 import 'tailwindcss/tailwind.css'
 import '@/utils/player.js'
 import '@/utils/metadata.js'
@@ -14,12 +10,6 @@ import '@/assets/main.css'
 
 const app = createApp(App)
 
-library.add(faUserSecret)
-
 app.use(store)
-
 app.use(router)
-
-app.component('font-awesome-icon', FontAwesomeIcon)
-
 app.mount('#app')
